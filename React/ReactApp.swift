aa22c88b4a6c3c48d18eaae12a16fe0fb04f72a2
Yannick Juarez <yannickjuarez@architech.agency>
2026-04-26 13:22:40 +0200

    Initial Commit

---
+++ React/ReactApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactApp.swift
+//  React
+//
+//  Created by Yannick Juarez on 26/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ReactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
